53b401fce65e1303bed9801c6d122437184c40d5
Ignacio Fernandez <ignacio.fernandez@gerdau.com>
2021-07-30 19:31:43 -0300

    todo index actualizado

--- src/components/screens/to-do-list/index.js
+++ src/components/screens/to-do-list/index.js
@@ -1,4 +1,24 @@
 import ToDoList from "./to-do-list"
-export default ToDoList
+//export default ToDoList
 
 
+  
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { addTodo, fetchTodos, editTodo, deleteTodo } from "../../../redux/thunks";
+import ToDoList from "./to-do-list";
+
+const mapStateToProps = state => ({
+    list: state.todos.list,
+    error: state.todos.error,
+    isLoading: state.todos.isLoading
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+    fetchTodos,
+    editTodo,
+    deleteTodo,
+    addTodo
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(ToDoList);
