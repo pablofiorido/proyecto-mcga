c75ab4d32b5ad774075d0606fab7404667a3408b
Pablo Fiorido <pablofiorido@hotmail.com>
2021-08-13 18:27:03 -0300

    deploy a heroku

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,4 +1,4 @@
-export const apiUrl =  "http://localhost:5050/api";
+export const apiUrl =  "http://localhost:5050/api" || "https://to-do-list-mcgaback.herokuapp.com/api";
 
 
 //export const apiUrl = "https://to-do-list-mcgaback.herokuapp.com/api"
